ff43924b8a6556fea38dd3a1aa41ec279923e053
Kushagra Agarwal <kushagraagarwal2003@gmail.com>
2025-11-05 04:20:29 +0530

    let's check now

--- backend/app.py
+++ backend/app.py
@@ -43,6 +43,7 @@ app.add_middleware(
             "http://127.0.0.1:5500",
             "http://localhost:5500",
             "http://localhost:3000",
+            "https://blocksentinel.netlify.app",
     ],
     allow_credentials=True,
     allow_methods=["*"],
